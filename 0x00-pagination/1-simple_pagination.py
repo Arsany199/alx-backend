027331ae8e4a021d410b3338abed27d44059ce89
arsany199 <arsanykamal30@gmail.com>
2024-06-23 02:50:56 -0700

    1-s

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""model that adds get method to server class"""
+"""add get_page method to class"""
 
 import csv
-import math
-from typing import List
+from typing import List, Tuple
 
 
 class Server:
@@ -26,13 +25,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """get items for page and page size"""
-            assert type(page) == int and type(page_size) == int
-            assert page > 0 and page_size > 0
-            start, end = self.index_range(page, page_size)
-            return (self.dataset()[start:end])
+        """finds the correct indexes to paginate the dataset"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        csv_size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """function that return a tuple of size two containing a start index
-    and an end index corresponding to the range of indexes"""
-    return (page - 1) * page_size, page * page_size
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple containing a start and end index"""
+    return ((page - 1) * page_size, page * page_size)
