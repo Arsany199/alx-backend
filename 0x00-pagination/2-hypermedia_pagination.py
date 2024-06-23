ac9248a4cb24dd8c9bba357baf9a9849cf1dd4b9
arsany199 <arsanykamal30@gmail.com>
2024-06-23 03:28:39 -0700

    2-hypermedia_pagination.py

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""add get_page method to class"""
+
+import math
+import csv
+from typing import List, Tuple, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """finds the correct indexes to paginate the dataset"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        csv_size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[str, Any]:
+        """returns a dictionary containing the following key-value pairs"""
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1
+            if page + 1 <= math.ceil(len(self.dataset()) / page_size) else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": math.ceil(len(self.dataset()) / page_size)
+        }
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple containing a start and end index"""
+    return ((page - 1) * page_size, page * page_size)
