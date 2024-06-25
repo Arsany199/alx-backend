6e3d475322b5264673ea4e415cff36465c1833fe
arsany199 <arsanykamal30@gmail.com>
2024-06-25 11:28:48 -0700

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,13 +13,16 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """put something in the cache"""
-        if key is not None or item is not None:
+        if key is None or item is None:
+            pass
+        else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
                     and key not in self.cache_data:
                 dis_key = list(self.cache_data.keys())[0]
                 del self.cache_data[dis_key]
                 print("DISCARD: {}".format(dis_key))
             self.cache_data[key] = item
+
     def get(self, key):
         """function to get value in self.cache_data"""
         if key is None or key not in self.cache_data.keys():
