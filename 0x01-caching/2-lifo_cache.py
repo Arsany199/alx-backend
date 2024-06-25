369feff18c8d25d923b5da07feca04f75b04e60a
arsany199 <arsanykamal30@gmail.com>
2024-06-25 11:40:30 -0700

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""model of lifo class"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """the class of lifo cache"""
+    def __init__(self):
+        """initialize from the parent class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put something in the cache"""
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                dis_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[dis_key]
+                print(f"DISCARD: {dis_key}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """function to get value in self.cache_data"""
+        if key is None or key not in self.cache_data:
+            return (None)
+        return (self.cache_data.get[key])
