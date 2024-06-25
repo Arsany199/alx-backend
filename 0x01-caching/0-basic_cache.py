6dfda42a192ec65f488e835c8c1b71cb1473a22a
arsany199 <arsanykamal30@gmail.com>
2024-06-25 10:36:36 -0700

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -21,4 +21,4 @@ class BasicCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return (None)
         else:
-            return (self.cache_data[key])
+            return (self.cache_data.get(key))
