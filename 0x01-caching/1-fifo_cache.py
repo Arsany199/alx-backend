21450f8d9aa1a0aa0a9aa85712707217813a4c5f
arsany199 <arsanykamal30@gmail.com>
2024-06-25 06:57:14 -0700

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""model of fifo class"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """the class of fifo cache"""
+    def __init__(self):
+        """initialize from the parent class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put something in the cache"""
+        if key != None or item != None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                dis_key = list(self.cache_data.keys())[0]
+                del self.cache_data[dis_key]
+                print("DISCARD: {}".format(dis_key))
+            self.cache_data[key] = item
+    def get(self, key):
+        """function to get value in self.cache_data"""
+        if key == None or key not in self.cache_data.keys():
+            return (None)
+        return (self.cache_data.get(key))
