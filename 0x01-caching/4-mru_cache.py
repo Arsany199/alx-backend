f15ae90d8d8a198b4d60023db2e8a1e8756a5843
arsany199 <arsanykamal30@gmail.com>
2024-06-25 13:30:54 -0700

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -16,7 +16,7 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         """puts item in the cache"""
         if key is None or item is None:
-            pass
+            return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 mru_key, _ = self.cache_data.popitem(False)
