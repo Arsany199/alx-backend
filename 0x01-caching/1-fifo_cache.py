37661e0df74d89ba54c945e8fa16f2207bc8fe72
arsany199 <arsanykamal30@gmail.com>
2024-06-25 11:31:27 -0700

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,11 +20,11 @@ class FIFOCache(BaseCaching):
                     and key not in self.cache_data:
                 dis_key = list(self.cache_data.keys())[0]
                 del self.cache_data[dis_key]
-                print("DISCARD: {}".format(dis_key))
+                print(f"DISCARD: {dis_key}")
             self.cache_data[key] = item
 
     def get(self, key):
         """function to get value in self.cache_data"""
-        if key is None or key not in self.cache_data.keys():
+        if key is None or key not in self.cache_data:
             return (None)
-        return (self.cache_data.get(key))
+        return (self.cache_data[key])
