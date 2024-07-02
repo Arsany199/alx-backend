4b2c415ed4974916ca5ab9f15870fe18c30b510b
Arsany199 <arsanykamal30@gmail.com>
2024-07-02 15:03:27 +0000

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""model to make a basic babel flask"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """defines the config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """function to determine the best match with the supported lang."""
+    support_lang = set(app.config["LANGUAGES"])
+    if i in support_lang:
+        return i
+    return request.accept_language.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index():
+    """return a template in html file"""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
