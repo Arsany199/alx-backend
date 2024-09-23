ccc5c510e0202da9138731d574daf2b7774ac46b
Arsany199 <arsanykamal30@gmail.com>
2024-09-23 22:09:55 +0300

    7-job_processor.js

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,7 +1,7 @@
 import { createQueue } from 'kue';
 
 const blacklistedNumbers = ['4153518780', '4153518781'];
-const queue = createQueue();
+const myqueue = createQueue();
 
 function sendNotification(phoneNumber, message, job, done) {
   job.progress(0, 100);
@@ -14,7 +14,7 @@ function sendNotification(phoneNumber, message, job, done) {
   done();
 }
 
-queue.process('push_notification_code_2', 2, (job, done) => {
+myqueue.process('push_notification_code_2', 2, (job, done) => {
   const { phoneNumber, message } = job.data;
   sendNotification(phoneNumber, message, job, done);
 });
